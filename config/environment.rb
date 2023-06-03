a8b1a5cef80d51669d3596bfba4d1deef9904988
Ronkibet <ronniek625@gmail.com>
2023-06-03 21:23:24 +0300

    test done

---
+++ config/environment.rb
@@ -0,0 +1,11 @@
+ENV['RACK_ENV'] ||= 'development'
+
+require 'bundler/setup'
+
+Bundler.require(:default, ENV['RACK_ENV'])
+
+
+
+require_relative "../app/models/product.rb"
+require_relative "../app/models/user.rb"
+require_relative "../app/models/review.rb"
